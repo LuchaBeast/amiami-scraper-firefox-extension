277eb9cfae4bcb42e081bf692a5e4a988aa0df7c
Drew Shamrock <drewshamrock@gmail.com>
2019-10-07 22:34:04 -0700

    Add logging for started and failed downloads

--- scrape_images.js
+++ scrape_images.js
@@ -1,13 +1,24 @@
-// content script
+// Content script
 
-// create arbitrary code block to limit scope of image_list
-// and avoid redeclaration error every time extension is run
+/** 
+ * create arbitrary code block to limit scope of image_list
+ * and avoid redeclaration error every time extension is run
+ */
 {
-    let image_list = document.querySelectorAll("ul.item-image__list > li > img");
-    let product_name = document.querySelector("h2.item-detail__section-title").innerHTML.replace("(Pre-order)","").replace(/[1-9][0-9]*\/[1-9][0-9]*/g,"").replace("Complete ","").replace("  "," ");
+    const image_list = document.querySelectorAll("ul.item-image__list > li > img");
+    
+    // Remove "Pre-order" and "Complete" from the product name because they do not contribute to the SEO of the product
+    // Remove "/" (if exists) and scale (i.e. 1/8, 1/7) from product name because Linux does not allow '/' in file names
+    const product_name = document.querySelector("h2.item-detail__section-title").innerHTML.replace("(Pre-order)","").replace(/[1-9][0-9]*\/[1-9][0-9]*/g,"").replace(/\//g," ").replace("Complete ","").replace("  "," ");
 
+    // Loop over each image and send the values necessary to construct the file name to the background script
     for (let image_counter = 0; image_counter < image_list.length; image_counter++) {
-        browser.runtime.sendMessage({"image_index": image_counter, "name": product_name, "image": image_list[image_counter].src, "file_ext": image_list[image_counter].src.split('.').pop()})
+        browser.runtime.sendMessage({
+            "image_index": image_counter,
+            "name": product_name,
+            "image": image_list[image_counter].src,
+            "file_ext": image_list[image_counter].src.split('.').pop()
+        })
     }
 
 }
--- scraper.js
+++ scraper.js
@@ -1,15 +1,31 @@
-// background script
+// Background script
 
+// Execute only when extension button is clicked
 browser.browserAction.onClicked.addListener((tab) => {
+    
     browser.tabs.executeScript({
         file: "/scrape_images.js",
         allFrames: true
     });
 
-    browser.runtime.onMessage.addListener(returned_images);
+    browser.runtime.onMessage.addListener(returned_image_urls);
 
-    function returned_images(message) {
-        browser.downloads.download({url: message.image, filename: "scraped_images/"+message.name+" "+message.image_index+"."+message.file_ext, saveAs: false});
+    function returned_image_urls(message) {
+        var downloading = browser.downloads.download({
+            url: message.image,
+            filename: "scraped_images/" + message.name + " " + message.image_index + "." + message.file_ext,
+            saveAs: false
+        });
+
+        downloading.then(onStartedDownload, onFailedDownload);
+    }
+
+    function onStartedDownload(id) {
+        console.log(`Started downloading: ${id}`);
+    }
+
+    function onFailedDownload(error) {
+        console.log(`Download failed: ${error}`);
     }
 
 });
